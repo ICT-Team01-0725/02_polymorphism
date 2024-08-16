3a4efcb63dfc643a6bd15029d955ee6044c95690
taekyoung-hong <clsrnvk@gmail.com>
2024-08-16 17:28:36 +0900

    Add files via upload

--- EX_main.java
+++ EX_main.java
@@ -5,10 +5,10 @@ public class EX_main {
 	
 	public static void main(String[] args) {
 
+		
 	     EX02 rectangle = new EX02();
 	     EX03 ellipse = new EX03();
 	     EX04 line = new EX04();
-	     
 	     rectangle.draw();
 	     ellipse.draw();
 	     line.draw();
